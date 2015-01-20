f7663876a99a1a982cf9639ee93f5e4f07abe74f
Alexandre Parra Betioli <apbetioli@gmail.com>
2015-01-20 13:12:22 -0200

    Running and Infinite background

---
+++ Assets/BackgroundMovement.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+var velocity : float = 10;
+
+function Start () {
+
+}
+
+function Update () {
+
+	if(Input.GetMouseButtonDown(0)) {
+		var mat = renderer.material;
+		var offset = mat.mainTextureOffset;
+		offset.y += Time.deltaTime * velocity;
+		mat.mainTextureOffset = offset;
+	}
+}
